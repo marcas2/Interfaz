f0e3fe4e4856e9725467493b561eb0cdbfff3fb8
Maria Casanova <marcas2>
2023-04-24 16:19:19 -0500

    Arreglar codigo y comentarlo

--- InterfazRMI/src/Interface/RMIDAO.java
+++ InterfazRMI/src/Interface/RMIDAO.java
@@ -7,13 +7,44 @@ package Interface;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 /**
- *
+ * Clase que implementa el DAO
  * @author maria
  */
 public interface RMIDAO extends Remote {
+    
+    /**
+     * Declaración metodo compras
+     * @param id
+     * @param num
+     * @return
+     * @throws RemoteException 
+     */
+    
     public String Compras(int id, int num)throws RemoteException;
+    
+    /**
+     * Declaración metodo VerCarrito
+     * @return
+     * @throws RemoteException 
+     */
+    
     public String VerCarrito()throws RemoteException;
+    
+    /**
+     * Declaración metodo VaciarCarrito
+     * @return
+     * @throws RemoteException 
+     */
+    
     public String VaciarCarrito () throws RemoteException;
+    
+    /**
+     * Declaración metodo VerificarUsuarios
+     * @param usuario
+     * @param contraseña
+     * @return
+     * @throws RemoteException 
+     */
     public boolean VerificarUsuarios (String usuario, String contraseña) throws RemoteException;
    
 }
